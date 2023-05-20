0e6d712d35e5ece8ec552ca678b23d2087ded500
bodigam naveen <naveenbodigam866@gmail.com>
2023-05-20 22:49:12 +0530

    i have added prime number code

---
+++ demo.py
@@ -0,0 +1,28 @@
+
+# # intersection in sets
+# a= {10,20,30,40,50}
+# b={20,80,30,40,70,100}
+# print(a.union (b))
+# # using intersection
+# a={10,20,30,40,50}
+# b={50,60,70,20,10}
+# print(a.intersection(b))
+# # using difference
+# a={10,20,30,40,50}
+# b={20,30,50,80,97}
+# print(a.difference(b))
+# # using dictionary
+# student = {"name":"naveen","section":"a","roll no":"21","class":"civil"}
+# print(student)
+# navi ={"navi":"karthik","raju":"kavi"}
+# print(student["class"])
+# #using dictionary methid
+# information["channel"]="bodigam naveen"
+# information["lkji"]="true"
+# print(information)
+# practice
+a=[1,2,2,4,5,7,8,9,10]
+b=[10,20,35,34,45,34]
+while print(print(print(print(a + b)):
+    WHILE
+    
